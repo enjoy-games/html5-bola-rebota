d60ad5a4bf952d60fb9e97c659c0dff9b34a13ac
sergio <sergio@localhost>
2012-09-08 16:42:32 +0200

    Inicio del proyecto: Estructura básica.

---
+++ js/requestAnimationFrame.js
@@ -0,0 +1,69 @@
+/*
+                                ESPAÑOL
+
+  Este archivo es parte de 'html5-bola-rebota'.
+
+  Copyright 2012 Sergio Lindo - <sergiolindo.empresa@gmail.com>
+
+  'html5-bola-rebota' es software libre: usted puede redistribuirlo y/o
+  modificarlo bajo los términos de la Licencia Pública General GNU publicada por
+  la Fundación para el Software Libre, ya sea la versión 3 de la Licencia,o (a
+  su elección) cualquier versión posterior.
+
+  'html5-bola-rebota' se distribuye con la esperanza de que sea útil, pero SIN
+  GARANTÍA ALGUNA; ni siquiera la garantía implícita MERCANTIL o de APTITUD PARA
+  UN PROPÓSITO DETERMINADO. Consulte los detalles de la Licencia Pública General
+  GNU para obtener una información más detallada.
+
+  Debería haber recibido una copia de la Licencia Pública General GNU junto a
+  'html5-bola-rebota'. En caso contrario, consulte
+  <http://www.gnu.org/licenses/>.
+
+
+                                ENGLISH
+
+  This file is part of 'html5-bola-rebota'.
+
+  Copyright 2012 Sergio Lindo - <sergiolindo.empresa@gmail.com>
+
+  'html5-bola-rebota' is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the Free
+  Software Foundation, either version 3 of the License, or (at your option) any
+  later version.
+
+  'html5-bola-rebota' is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  'html5-bola-rebota'. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// Bucle optimizado para animaciones web.
+(function() {
+	var lastTime = 0;
+	var vendors = ['ms', 'moz', 'webkit', 'o'];
+
+	for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+		window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+		window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] ||
+									  window[vendors[x]+'CancelRequestAnimationFrame'];
+	}
+
+	if (!window.requestAnimationFrame)
+		window.requestAnimationFrame = function(callback, element) {
+			var currTime = new Date().getTime();
+			var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+			var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
+			lastTime = currTime + timeToCall;
+			return id;
+	};
+
+	if (!window.cancelAnimationFrame)
+		window.cancelAnimationFrame = function(id) { clearTimeout(id); };
+}());
+
+// Información en consola javascript del navegador.
+console.info("Incluído requestAnimationFrame.js");
